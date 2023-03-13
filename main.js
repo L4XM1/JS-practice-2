782194f33aa804fbecdb23a321988c5a2e15fb00
Marta Maksimovic <marta.maksimovic1988@gmail.com>
2023-03-14 00:26:21 +0100

    feat: form

--- main.js
+++ main.js
@@ -290,15 +290,54 @@ items.forEach((item) => console.log(item));
 
 const ul = document.querySelector(".items");
 // ul.remove();
-ul.lastElementChild.remove();
-ul.firstElementChild.textContent = "Hello";
-ul.children[1].innerText = "Marta";
-ul.lastElementChild.innerHTML = "<h1>Hello</h1>";
+// ul.lastElementChild.remove();
+// ul.firstElementChild.textContent = "Hello";
+// ul.children[1].innerText = "Marta";
+// ul.lastElementChild.innerHTML = "<h1>Hello</h1>";
 
 const btn = document.querySelector(".btn");
 btn.style.background = "green";
 
 btn.addEventListener("click", (e) => {
-  e.preventDefault();
-  console.log(e.target.className);
+  // e.preventDefault();
+  // console.log(e.target.className);
+  document.querySelector("#my-form").style.background = "#ccc";
+  document.querySelector("body").classList.add("bg-dark");
+  // ul.lastElementChild.innerHTML = "<h1>Adding hello from js</h1>";
 });
+
+//form
+
+const myForm = document.querySelector("#my-form");
+const nameInput = document.querySelector("#name");
+const emailInput = document.querySelector("#email");
+const msg = document.querySelector(".msg");
+const userList = document.querySelector("#users");
+
+myForm.addEventListener("submit", onSubmit);
+
+function onSubmit(e) {
+  e.preventDefault();
+
+  console.log(nameInput.value);
+
+  if (nameInput.value === "" || emailInput.value === "") {
+    msg.classList.add("error");
+    msg.innerHTML = "Please enter all fields";
+
+    setTimeout(() => {
+      msg.innerHTML = "";
+      msg.classList.remove("error");
+    }, 3000);
+  } else {
+    const li = document.createElement("li");
+    li.appendChild(
+      document.createTextNode(`${nameInput.value} : ${emailInput.value}`)
+    );
+    userList.appendChild(li);
+
+    //clear fields
+    nameInput.value = "";
+    emailInput.value = "";
+  }
+}
